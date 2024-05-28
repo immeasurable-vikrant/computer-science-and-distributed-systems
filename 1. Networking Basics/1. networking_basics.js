d942e1eec8957fc2d6c5addf37b06e0ce511bd9b
vikrant <vikrant@admins-MacBook-Pro-2.local>
2024-05-28 20:49:49 +0530

    Initial commit - Networking basics

---
+++ 1. Networking Basics/1. networking_basics.js
@@ -0,0 +1,36 @@
+`
+Networking:
+
+- What is a Network?
+        A network is a collection of interconnected devices, such as computers, servers, and other hardware, 
+        that communicate with each other to share resources and data.
+
+- What is Networking?
+        Networking refers to the practice of connecting and managing these devices to enable communication, 
+        data exchange, and resource sharing between them.
+
+- What is Computer Networking?
+        Computer networking is the specific practice of connecting computers and related devices using 
+        wired or wireless methods to facilitate communication and resource sharing. It involves both 
+        hardware (e.g., routers, switches) and software (e.g., protocols, network management tools).
+
+- Why Do We Need Computer Networking?
+        - Resource Sharing: Allows sharing of hardware (printers, scanners) and software resources (files, applications).
+        - Communication: Facilitates communication through emails, messaging, and video conferencing.
+        - Data Transfer: Enables efficient and quick transfer of data between devices.
+        - Centralized Management: Allows for centralized administration and management of devices and data.
+        - Scalability and Flexibility: Supports the growth and adaptation of systems to meet evolving needs.
+
+When Did Computer Networking Happen?
+        - Early Development: Began in the 1960s with the creation of the ARPANET, the precursor to the internet.
+        - Commercial Adoption: Expanded in the 1980s and 1990s with the rise of personal computers and the commercial internet.
+
+How Does Computer Networking Work?
+
+        - Physical Layer: Involves the actual hardware connections using cables (Ethernet) or wireless signals (Wi-Fi).
+        - Data Link Layer: Manages the node-to-node data transfer (Ethernet, Wi-Fi).
+        - Network Layer: Handles data routing and addressing (IP addresses).
+        - Transport Layer: Ensures reliable data transfer (TCP) or faster, less reliable transfer (UDP).
+        - Application Layer: Interfaces directly with the end-user applications (HTTP, FTP).      
+
+These layers work together to allow devices to communicate, share resources, and transfer data efficiently across networks.`
